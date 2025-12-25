516ce3c1a52d650b33985c256e1df7a9f9ab8fd1
Mateusz Grabowski <mat.grabowski@icloud.com>
2025-12-25 21:01:42 +0100

    add username to user model

--- app/models/user.py
+++ app/models/user.py
@@ -7,6 +7,7 @@ class User(Base):
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
