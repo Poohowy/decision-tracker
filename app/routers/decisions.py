a98841e5a72775366350410a1ce5c431005af937
Mateusz Grabowski <mat.grabowski@icloud.com>
2025-12-26 15:14:55 +0100

    add service layer; move business logic from router to service layer

--- app/routers/decisions.py
+++ app/routers/decisions.py
@@ -1,50 +1,29 @@
 from typing import List
-
-from fastapi import Depends, APIRouter, HTTPException
+from fastapi import Depends, APIRouter, status
 from sqlalchemy.orm import Session
+
 from app.db.session import get_db
-from app.models import Decision
 from app.schemas.decision import DecisionCreate, DecisionRead
+from app.services.decision_service import create_decision, get_decision_by_id, list_decisions
 
 router = APIRouter(prefix="/decision")
 
 @router.get("", response_model=List[DecisionRead])
-def get_decisions(
+def list_decisions_endpoint(
     db: Session = Depends(get_db)
-):
-    decisions = (
-        db.query(Decision)
-        .offset(0)
-        .limit(20)
-        .all()
-    )
-    return decisions
+) -> List[DecisionRead]:
+    return list_decisions(db=db)
 
 @router.get("/{id}", response_model=DecisionRead)
-def get_decision(decision_id: int, db: Session = Depends(get_db)):
-    decision = db.get(Decision, decision_id)
+def get_decision_by_id_endpoint(
+    decision_id: int,
+    db: Session = Depends(get_db)
+) -> DecisionRead:
+    return get_decision_by_id(decision_id=decision_id, db=db)
 
-    if not decision:
-        raise HTTPException(
-            status_code=404,
-            detail="Decision not found",
-        )
-
-    return decision
-
-@router.post("", response_model=DecisionRead)
-def create_decision(data: DecisionCreate, db: Session = Depends(get_db)):
-    decision = Decision(
-        owner_id=data.owner_id,
-        title=data.title,
-        description=data.description,
-        alternatives=data.alternatives,
-        final_decision=data.final_decision,
-        expectations=data.expectations,
-    )
-
-    db.add(decision)
-    db.commit()
-    db.refresh(decision)
-
-    return decision
+@router.post("", response_model=DecisionRead, status_code=status.HTTP_201_CREATED)
+def create_decision_endpoint(
+    data: DecisionCreate,
+    db: Session = Depends(get_db)
+) -> DecisionRead:
+    return create_decision(data=data, db=db)
---
+++ app/services/decision_service.py
@@ -0,0 +1,45 @@
+from typing import List
+
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.db.session import get_db
+from app.models import Decision
+from app.schemas.decision import DecisionCreate, DecisionRead
+
+def list_decisions(
+    db: Session = Depends(get_db)
+):
+    decisions = (
+        db.query(Decision)
+        .offset(0)
+        .limit(20)
+        .all()
+    )
+    return decisions
+
+def get_decision_by_id(decision_id: int, db: Session = Depends(get_db)):
+    decision = db.get(Decision, decision_id)
+
+    if not decision:
+        raise HTTPException(
+            status_code=404,
+            detail="Decision not found",
+        )
+
+    return decision
+
+def create_decision(data: DecisionCreate, db: Session = Depends(get_db)):
+    decision = Decision(
+        owner_id=data.owner_id,
+        title=data.title,
+        description=data.description,
+        alternatives=data.alternatives,
+        final_decision=data.final_decision,
+        expectations=data.expectations,
+    )
+
+    db.add(decision)
+    db.commit()
+    db.refresh(decision)
+
+    return decision
